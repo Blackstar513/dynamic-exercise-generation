ead6dddd7b874ea954b8afc35e1acc5539b91164
Malte Koch <malte.koch@stud.fra-uas.de>
2022-04-27 12:31:30 +0200

    added all database tables to the admin site

--- dynexgen/exgen/admin.py
+++ dynexgen/exgen/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Exercise, Answer, Category, Course, Lecturer, ExercisePicture, AnswerPicture, ExerciseDependency, \
+    CourseExercise, CourseCategory, ExerciseCategory
+
 
 # Register your models here.
+admin.site.register(Exercise)
+admin.site.register(Answer)
+admin.site.register(Category)
+admin.site.register(Course)
+admin.site.register(Lecturer)
+admin.site.register(ExercisePicture)
+admin.site.register(AnswerPicture)
+admin.site.register(ExerciseDependency)
+admin.site.register(CourseExercise)
+admin.site.register(CourseCategory)
+admin.site.register(ExerciseCategory)
