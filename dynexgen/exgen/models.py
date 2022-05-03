652bf903093d56cda361f4bed920f29fca132e2c
Malte Koch <malte.koch@stud.fra-uas.de>
2022-05-03 18:30:13 +0200

    dependency order in Exercise model is now correct; added functionality to get all parent dependencies of an exercise

--- dynexgen/exgen/models.py
+++ dynexgen/exgen/models.py
@@ -26,9 +26,69 @@ class Exercise(models.Model):
     text = models.TextField(verbose_name="Aufgabentext")
     creator = models.ForeignKey(Lecturer, related_name='exercises', on_delete=models.SET_NULL, blank=True, null=True,
                                 verbose_name="Ersteller")
-    dependency = models.ManyToManyField('self', through='ExerciseDependency', through_fields=('parent', 'child'),
+    dependency = models.ManyToManyField('self', through='ExerciseDependency', through_fields=('child', 'parent'),
                                         symmetrical=False, blank=True, verbose_name="Abhängig von")
     category = models.ManyToManyField('Category', through='ExerciseCategory', verbose_name="Kategorien")
+
+    def get_all_parent_dependencies(self):
+        
+        # list of all even dependencies
+        even_dependencies = []
+        # list of all higher dependencies
+        higher_dependencies = []
+
+        # get all dependencies where self is the child recursively
+        for d in self.children.all():
+            parent_dependencies = d.parent.get_all_parent_dependencies()
+
+            if d.hierarchy == 'higher':
+                higher_dependencies.extend(parent_dependencies)
+            else:
+                even_dependencies.extend(parent_dependencies)
+
+        # list of all parent dependencies
+        dependencies = []
+
+        # append higher dependencies if there are any
+        if higher_dependencies:
+            dependencies.append(tuple(higher_dependencies))
+        # keep even dependencies on same level as self
+        dependencies.extend(even_dependencies)
+        # add self
+        dependencies.append(self)
+        # ensure that no dependency doubling exists (as long as there is no weird exercise setup)
+        return list(dict.fromkeys(tuple(dependencies)))
+
+    def get_all_parent_dependencies_correctly_nested(self):
+        def reverse_nesting(nested_list, nesting_level=0):
+            new_list = []
+            deep_list = []
+            if not (isinstance(nested_list, list) or isinstance(nested_list, tuple)):
+                return nesting_level + 1, nested_list
+
+            for elem in nested_list:
+                if isinstance(elem, list) or isinstance(elem, tuple):
+                    nesting_level, result_list = reverse_nesting(elem, nesting_level)
+                    new_list.extend(result_list)
+                else:
+                    deep_list.append(elem)
+
+            # for _ in range(nesting_level):
+            #    deep_list = [deep_list]
+            temp = new_list
+            for _ in range(nesting_level - 1):
+                temp = temp[-1]
+
+            if nesting_level == 0:
+                temp.extend(deep_list)
+            else:
+                temp.append(deep_list)
+            return nesting_level + 1, new_list
+
+        parent_dependencies = self.get_all_parent_dependencies()
+        _, reverse_nested_dependencies = reverse_nesting(parent_dependencies)
+
+        return reverse_nested_dependencies
 
     def __str__(self):
         return f"{self.text[:20]}..."
