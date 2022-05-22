15871196ffa166bba9a2bbd1daac84d38e00fee1
Malte <maltekoch96@gmx.de>
2022-05-22 17:48:37 +0200

    added exercise search page to urls

--- dynexgen/exgen/urls.py
+++ dynexgen/exgen/urls.py
@@ -6,5 +6,6 @@ app_name = 'exgen'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:pk>/exercise', views.ExerciseBottomUpView.as_view(), name='bottom_up')
+    path('<int:pk>/exercise', views.ExerciseBottomUpView.as_view(), name='bottom_up'),
+    path('exercise_search', views.search_for_exercises, name='exercise_search')
 ]
