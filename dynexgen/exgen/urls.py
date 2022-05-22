d5de08cf65144862b154806c1d697db1d86c42db
Malte <maltekoch96@gmx.de>
2022-05-22 16:02:21 +0200

    set app_name in urls.py

--- dynexgen/exgen/urls.py
+++ dynexgen/exgen/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'exgen'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:pk>/exercise', views.ExerciseBottomUpView.as_view(), name='bottom_up')
