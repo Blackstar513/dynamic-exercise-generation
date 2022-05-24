7c6c5f97ada6f0102f38ec61345fe99cfd03af23
Malte Koch <malte.koch@stud.fra-uas.de>
2022-05-24 09:15:53 +0200

    added exercise answer view to urls

--- dynexgen/exgen/urls.py
+++ dynexgen/exgen/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:pk>/exercise', views.ExerciseBottomUpView.as_view(), name='bottom_up')
+    path('<int:pk>/exercise', views.ExerciseBottomUpView.as_view(), name='bottom_up'),
+    path('<int:pk>/exercise_answers', views.ExerciseAnswerBottomUpView.as_view(), name='answer_bottom_up')
 ]
