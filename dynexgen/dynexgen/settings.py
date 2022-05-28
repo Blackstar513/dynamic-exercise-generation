8280f26fef8280da98e6d20a5f44f434915b926f
Malte Koch <malte.koch@stud.fra-uas.de>
2022-05-28 15:47:57 +0200

    added a MEDIA_ROOT directory for Debug and non Debug mode

--- dynexgen/dynexgen/settings.py
+++ dynexgen/dynexgen/settings.py
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+if DEBUG:
+    MEDIA_ROOT = os.path.abspath('./media/')
+else:
+    MEDIA_ROOT = '/var/www/django/media'
