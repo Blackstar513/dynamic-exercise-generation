33d9606265aae9469b6680b691ea5e5fb81322ee
Malte Koch <malte.koch@stud.fra-uas.de>
2022-06-07 18:45:25 +0200

    prettified exercise overview panel; added filter options to the exercise overview panel; added a search options to the exercise overview panel; added make_published and make_unpublished actions to the exercise overview panel;

--- dynexgen/exgen/admin.py
+++ dynexgen/exgen/admin.py
@@ -3,7 +3,18 @@ from .models import Exercise, Answer, Category, Course, Lecturer, ExercisePictur
     CourseExercise, CourseCategory, ExerciseCategory
 
 
-# Register your models here.
+# Custom admin actions
+@admin.action(description='Mark selected exercises as published')
+def make_published(modeladmin, request, queryset):
+    queryset.update(published=True)
+
+
+@admin.action(description='Mark selected exercises as unpublished')
+def make_unpublished(modeladmin, request, queryset):
+    queryset.update(published=False)
+
+
+# Inline elements
 class ExerciseInline(admin.TabularInline):
     model = ExerciseDependency
     extra = 0
@@ -35,7 +46,13 @@ class CourseCategoryInline(admin.TabularInline):
     extra = 1
 
 
+# Custom admin forms
 class ExerciseAdmin(admin.ModelAdmin):
+    list_display = ('text', 'creator', 'text_type', 'published')
+    list_filter = ('creator', 'published', 'text_type')
+    search_fields = ('text', 'creator__surname__startswith', 'text_type__startswith')
+    actions = (make_published, make_unpublished)
+
     fieldsets = [
         (None,               {'fields': ['text', 'text_type']}),
         ('Lecturer', {'fields': ['creator']}),
@@ -48,6 +65,7 @@ class CourseAdmin(admin.ModelAdmin):
     inlines = [CourseCategoryInline]
 
 
+# Register your models here.
 admin.site.register(Exercise, ExerciseAdmin)
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lecturer)
