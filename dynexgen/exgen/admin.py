40e11c0046a1a8f37fe2b811a687e40e488abf0c
Malte Koch <malte.koch@stud.fra-uas.de>
2022-06-08 20:29:55 +0200

    assemblies can now be created; exercise panel can now filter by date_modified; exercise panel now display a short version of comment with a hover mechanic; exercise add/change added collapsable comment;

--- dynexgen/exgen/admin.py
+++ dynexgen/exgen/admin.py
@@ -1,7 +1,8 @@
 import nested_admin
+from django.utils.html import format_html
 from django.contrib import admin
 from .models import Exercise, Answer, Category, Course, ExercisePicture, AnswerPicture, ExerciseDependency, \
-    CourseExercise, CourseCategory, ExerciseCategory
+    CourseExercise, CourseCategory, ExerciseCategory, Assembly, ExerciseAssembly, AssemblyCategory
 
 
 # Custom admin actions
@@ -65,21 +66,61 @@ class CourseCategoryInline(admin.TabularInline):
     extra = 1
 
 
+class ExerciseAssemblyInline(admin.TabularInline):
+    model = ExerciseAssembly
+    extra = 1
+
+
+class AssemblyCategoryInline(admin.TabularInline):
+    model = AssemblyCategory
+    extra = 1
+
+
 # Custom admin forms
 class ExerciseAdmin(nested_admin.NestedModelAdmin):
-    list_display = ('__str__', 'creator', 'text_type', 'comment', 'date_modified', 'published')
+    list_display = ('__str__', 'creator', 'text_type', 'short_comment', 'date_modified', 'published')
     #list_display_links = list_display
-    list_filter = ('creator', 'published', 'text_type')
+    list_filter = ('creator', 'published', 'text_type', ('date_modified', admin.DateFieldListFilter))
+
     search_fields = ('title', 'text', 'creator__last_name__startswith', 'text_type__startswith')
     search_help_text = "Searches anywhere in titles and text;\nSearches if creator or texttype start with the query"
+
     actions = (make_published, make_unpublished, make_clone)
+
     save_as = True  # overwrites "save and add another" with "save as new" -> allows cloning of exercises
 
     fieldsets = [
         (None,               {'fields': ['title', 'text', 'text_type']}),
+        ("Comment",               {'fields': ['comment'],
+                              'classes': ['collapse']}),
         ('Published?', {'fields': ['published']}),
     ]
     inlines = [ExerciseInline, AnswerInline, ExercisePictureInline, ExerciseCategoryInline, CourseInline]
+
+    @admin.display(description="comment")
+    def short_comment(self, obj):
+        s_comment = obj.comment
+        if len(s_comment) > 150:
+            s_comment = f"{s_comment[:150]}..."
+        return format_html("<span title='{}'>{}</span>", obj.comment, s_comment)
+
+    def save_model(self, request, obj, form, change):
+        if not obj.creator:
+            obj.creator = request.user
+        super().save_model(request, obj, form, change)
+
+
+class AssemblyAdmin(admin.ModelAdmin):
+    list_display = ('title', 'creator', 'date_modified', 'published')
+
+    actions = (make_published, make_unpublished, make_clone)
+    save_as = True  # overwrites "save and add another" with "save as new" -> allows cloning of exercises
+
+    fieldsets = [
+        (None, {'fields': ['title']}),
+        ('Published?', {'fields': ['published']}),
+    ]
+    inlines = [ExerciseAssemblyInline, AssemblyCategoryInline]
 
     def save_model(self, request, obj, form, change):
         if not obj.creator:
@@ -90,5 +131,5 @@ class ExerciseAdmin(nested_admin.NestedModelAdmin):
 # Register your models here.
 admin.site.register(Exercise, ExerciseAdmin)
 admin.site.register(Course, inlines=[CourseCategoryInline])
-#admin.site.register(Answer, inlines=[AnswerPictureInline])
+admin.site.register(Assembly, AssemblyAdmin)
 admin.site.register(Category)
