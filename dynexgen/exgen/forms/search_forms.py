340f4b4e531159a860e912d6a8258cc8377cfd5c
Malte Koch <malte.koch@stud.fra-uas.de>
2022-07-04 10:01:39 +0200

    added static files from django admin; exercise_search now uses FilteredSelectMultipleWidget; exercise_search can now search for lecturer; exercise_search can now decide between all or any for category search; cleaned up search_for_exercises code;

--- dynexgen/exgen/forms/search_forms.py
+++ dynexgen/exgen/forms/search_forms.py
@@ -1,12 +1,20 @@
 from django import forms
-from ..models import Category, Exercise
+from ..models import Category, Course
+from django.contrib.auth import get_user_model
+from django.contrib.admin.widgets import FilteredSelectMultiple
+
+User = get_user_model()
 
 
 class ExerciseSearchForm(forms.Form):
-    text = forms.CharField(label="Aufgabentext", widget=forms.Textarea, required=False)
-    categories = forms.ModelMultipleChoiceField(label="Kategorien", queryset=Category.objects.all(), required=False)
+    text = forms.CharField(label="Exercise text", widget=forms.Textarea, required=False)
+    categories = forms.ModelMultipleChoiceField(label="Categories", queryset=Category.objects.all(),
+                                                widget=FilteredSelectMultiple("Categories", is_stacked=False),
+                                                required=False)
+    category_connect = forms.ChoiceField(label="Search must include", choices=(('all', "All"),
+                                                                               ('any', "Any")),
+                                         widget=forms.RadioSelect, initial='all', required=True)
+    lecturers = forms.ModelMultipleChoiceField(label="Lecturers", queryset=User.objects.all(),
+                                               widget=FilteredSelectMultiple("Lecturers", is_stacked=False),
+                                               required=False)
 
-# class ExerciseSearchForm(forms.ModelForm):
-#     class Meta:
-#         model = Exercise
-#         fields = ['text', 'category']
--- dynexgen/exgen/views/search_views.py
+++ dynexgen/exgen/views/search_views.py
@@ -6,24 +6,30 @@ from ..models import Exercise
 
 def search_for_exercises(request):
     search_results = None
-    if request.GET.get('text') or request.GET.get('categories'):
+    if request.GET.get('text') or request.GET.get('categories') or request.GET.get('lecturers'):
         text = request.GET.get('text')
         text = text if text is not None else ""
 
         categories = request.GET.getlist('categories')
+        lecturers = request.GET.getlist('lecturers')
+
+        category_include_all = request.GET.get('category_connect') == 'all'
+
+        query_list = [Q(text__icontains=text), Q(published=True)]
+        if lecturers:
+            query_list.append(Q(creator__pk__in=lecturers))
 
         if categories:
-            search_queries = []
-            for categorie in categories:
-                search_queries.append(Exercise.objects.filter(Q(text__icontains=text),
-                                                              Q(category__pk__exact=categorie)))
-
-            if len(search_queries) > 1:
-                search_results = search_queries[0].intersection(*search_queries[1:])
+            if category_include_all:
+                search_results = Exercise.objects.all()
+                for category in categories:
+                    search_results = search_results.filter(*query_list,
+                                                           Q(category__pk__exact=category))
             else:
-                search_results = search_queries[0]
+                search_results = Exercise.objects.filter(*query_list,
+                                                         Q(category__pk__in=categories))
         else:
-            search_results = Exercise.objects.filter(text__icontains=text).distinct()
+            search_results = Exercise.objects.filter(*query_list)
 
         form = ExerciseSearchForm(request.GET)
     else:
