16d810767d43825be7cea40f45e1a1170c7ebab0
Malte Koch <malte.koch@stud.fra-uas.de>
2022-07-06 15:29:23 +0200

    changed filetype to doctype; added SelectExerciseForm

--- dynexgen/exgen/forms/select_forms.py
+++ dynexgen/exgen/forms/select_forms.py
@@ -1,7 +1,30 @@
 from django import forms
-from ..models import Category, Exercise
+from django.contrib.admin.widgets import FilteredSelectMultiple
+
 
 class SelectFileTypeForm(forms.Form):
 
-    filetype=forms.ChoiceField(label="filetypes", choices=(("pdf","PDF"),("html","HTML"),("docx","DOCX"),("latex","LATEX")),widget=forms.RadioSelect(),initial="pdf",required=True)
-    full_exercise=forms.BooleanField(label="use full exercises?", required=False)
+    doctype = forms.ChoiceField(label="filetypes", choices=(('pdf', "PDF"),
+                                                             ('html', "HTML"),
+                                                             ('docx', "DOCX"),
+                                                             ('latex', "LATEX")),
+                                  widget=forms.RadioSelect(), initial='pdf',
+                                  required=True)
+
+    full_exercise = forms.BooleanField(label="use full exercises?", required=False)
+    exercises = forms.MultipleChoiceField(widget=forms.MultipleHiddenInput())
+    assembly = forms.ChoiceField(widget=forms.HiddenInput())
+
+
+class SelectExercisesForm(forms.Form):
+    def __init__(self, exercise_choices: list, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        choices = []
+        for exercise in exercise_choices:
+            categories = [str(c) for c in exercise.category.all()]
+            choices.append((exercise.id, f"{exercise} | {exercise.creator} | {','.join(categories)}"))
+        self.fields['exercises'] = forms.MultipleChoiceField(label="Exercises", choices=tuple(choices),
+                                                             widget=FilteredSelectMultiple("Exercises", is_stacked=False),
+                                                             required=True)
+
+
