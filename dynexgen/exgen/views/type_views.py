f586b699381b80988fdb38fe5398dc661c853014
pooya2008 <80066859+pooya2008@users.noreply.github.com>
2022-07-06 13:18:38 +0200

    Add files via upload

---
+++ dynexgen/exgen/views/type_views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.db.models import Q
+from ..forms.select_forms import SelectFileTypeForm
+from ..models import Exercise
+
+
+# Create your views here.
+def type_views(request):
+  
+  # render function takes argument - request
+  # and return HTML as response
+    return render(request, "exgen/type_selector.html",{"form":SelectFileTypeForm()})
