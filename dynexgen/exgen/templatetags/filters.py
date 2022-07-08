1242d8d8acee04d80e3154bae3eca03fe45f04ba
Malte Koch <malte.koch@stud.fra-uas.de>
2022-07-08 14:31:16 +0200

    added filter for indexing lists

--- dynexgen/exgen/templatetags/filters.py
+++ dynexgen/exgen/templatetags/filters.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.filter(name='range')
 def create_range(times: int):
     return range(times)
+
+
+@register.filter(name='index')
+def get_at_index(indexable, i):
+    return indexable[i]
